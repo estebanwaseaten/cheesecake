63e5276473cc310389e1743d8656bc52c55dd3e8
Daniel W <daniel.wegkamp@gmail.com>
2025-04-02 14:41:41 +0100

    with gitignore

--- cheesecake.c
+++ cheesecake.c
@@ -11,13 +11,17 @@ MODULE_AUTHOR("dw42");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0:1.0");
 
+int notify_callback( const char *val, const struct kernel_param *kp );
+static ssize_t custom_read( struct file* file, char __user* user_buffer, size_t count, loff_t* offset );
+
+int cb_valueETX = 0;
+
 static struct proc_dir_entry* my_proc_dir_entry;
 static const struct proc_ops pops =		//replace struct file_operations with struct proc_ops for kernel version 5.6 or later
 {
 	//.owner = THIS_MODULE,
 	.proc_read = custom_read		//what happens when you read /proc/cheesecakedrivercd
 };
-
 
 const struct kernel_param_ops my_param_ops =
 {
@@ -62,13 +66,13 @@ static int __init cheesecake_init(void)
 		pr_info("Installed Cheesecake :)\n");		//pr_emerg(), pr_alert(), pr_crit(), pr_err(), pr_warn(), pr_notice(), pr_info(), pr_debug()
 		my_proc_dir_entry = proc_create("cheesePI", 0666, NULL, &pops );
 
-		
+
 
 		return 0;
 }
 static void __init cheesecake_exit(void)
 {
-		proc_remove( my_proc_dir_entry );
+		remove_proc_entry( "cheesePI", NULL );
 		pr_info("Uninstalled Cheesecake :(\n");
 }
 module_init(cheesecake_init);
